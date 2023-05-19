5778cafc1006b2f4416bb2998ccf2eab69efd5b9
Ivy <28644397+ivynganga@users.noreply.github.com>
2023-05-19 11:21:27 +0300

    Deploy gh-pages

---
+++ _next/static/BJ5mtJ90imAO4rFKNRkoY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/407-910a94003783e7ed.js","static/chunks/pages/index-7618598c2c828df8.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
